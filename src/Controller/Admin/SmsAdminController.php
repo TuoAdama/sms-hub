1d66d0106f832322990c927325dadb5f1e97d75e
TuoAdama <tuoadama17@gmail.com>
2024-12-01 23:37:18 +0100

    feat: list message

--- src/Controller/Admin/SmsAdminController.php
+++ src/Controller/Admin/SmsAdminController.php
@@ -2,15 +2,28 @@
 
 namespace App\Controller\Admin;
 
+use App\Repository\SmsMessageRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 #[Route('/admin/sms')]
+//#[IsGranted('ROLE_ADMIN')]
 class SmsAdminController extends AbstractController
 {
+
+    public function __construct(
+        private readonly SmsMessageRepository $smsMessageRepository,
+    )
+    {
+    }
+
     #[Route('/', name: 'app_sms_index')]
     public function index(): Response {
-        return $this->render('pages/sms/sms-index.html.twig');
+        $sms = $this->smsMessageRepository->findAll();
+        return $this->render('pages/sms/sms-index.html.twig', [
+            'sms' => $sms,
+        ]);
     }
 }
