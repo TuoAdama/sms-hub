45bd65a8a49366fdb0c3c75883925787c47fb4b0
TuoAdama <tuoadama17@gmail.com>
2024-12-01 22:52:19 +0100

    feat: init admin lte

--- assets/app.js
+++ assets/app.js
@@ -1,9 +1,22 @@
-/*
- * Welcome to your app's main JavaScript file!
- *
- * We recommend including the built version of this JavaScript file
- * (and its CSS file) in your base layout (base.html.twig).
- */
-
-// any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.css';
+import "admin-lte/plugins/fontawesome-free/css/all.min.css"
+import "admin-lte/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css";
+import "admin-lte/plugins/icheck-bootstrap/icheck-bootstrap.min.css"
+import "admin-lte/plugins/jqvmap/jqvmap.min.css"
+import "admin-lte/dist/css/adminlte.min.css"
+import "admin-lte/plugins/overlayScrollbars/css/OverlayScrollbars.min.css"
+import "admin-lte/plugins/daterangepicker/daterangepicker.css"
+import "admin-lte/plugins/summernote/summernote-bs4.min.css"
+
+import "admin-lte/plugins/jquery/jquery.min.js"
+import "admin-lte/plugins/jquery-ui/jquery-ui.min.js"
+import "admin-lte/plugins/bootstrap/js/bootstrap.bundle.min.js"
+import "admin-lte/plugins/chart.js/Chart.min.js"
+import "admin-lte/plugins/sparklines/sparkline.js"
+import "admin-lte/plugins/jqvmap/jquery.vmap.min.js"
+import "admin-lte/plugins/jqvmap/maps/jquery.vmap.usa.js"
+import "admin-lte/plugins/jquery-knob/jquery.knob.min.js"
+import "admin-lte/plugins/daterangepicker/daterangepicker.js"
+import "admin-lte/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"
+import "admin-lte/dist/js/adminlte.js"
+import "admin-lte/dist/js/demo.js"
