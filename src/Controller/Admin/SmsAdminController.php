414170b584b0287f189756a128fb1d3c2bf84f6c
TuoAdama <tuoadama17@gmail.com>
2024-12-02 15:37:15 +0100

    feat: added authentification

--- src/Controller/Admin/SmsAdminController.php
+++ src/Controller/Admin/SmsAdminController.php
@@ -8,9 +8,10 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 #[Route('/admin/sms')]
-//#[IsGranted('ROLE_ADMIN')]
+#[IsGranted('ROLE_ADMIN')]
 class SmsAdminController extends AbstractController
 {
 
