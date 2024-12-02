6516da181930fd3141fbebe7a5025f2df6d80c99
TuoAdama <tuoadama17@gmail.com>
2024-12-02 15:16:29 +0100

    feat: added profiler debug

--- config/bundles.php
+++ config/bundles.php
@@ -10,4 +10,5 @@ return [
     SymfonyCasts\Bundle\VerifyEmail\SymfonyCastsVerifyEmailBundle::class => ['all' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
 ];
