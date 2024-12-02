63c4e0447f0a7cb4d3e273b7523b6e6726b66519
TuoAdama <tuoadama17@gmail.com>
2024-12-02 14:32:29 +0100

    feat: added user registration form

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -5,10 +5,13 @@ namespace App\Form;
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Validator\Constraints\IsTrue;
+use Symfony\Component\Validator\Constraints\Email;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
 
@@ -17,20 +20,46 @@ class RegistrationFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
-            ->add('agreeTerms', CheckboxType::class, [
-                'mapped' => false,
+            ->add('email', EmailType::class, [
+                'label' => false,
                 'constraints' => [
-                    new IsTrue([
-                        'message' => 'You should agree to our terms.',
-                    ]),
+                    new Email()
                 ],
+                'attr' => [
+                    'class' => 'form-control',
+                    "placeholder" => "Email"
+                ]
             ])
-            ->add('plainPassword', PasswordType::class, [
-                // instead of being set onto the object directly,
-                // this is read and encoded in the controller
+            ->add('name', TextType::class, [
+                'label' => false,
+                'required' => true,
+                'attr' => [
+                    'class' => 'form-control',
+                    "placeholder" => "form.lastname"
+                ]
+            ])
+            ->add('password', RepeatedType::class, [
+                'required' => true,
                 'mapped' => false,
-                'attr' => ['autocomplete' => 'new-password'],
+                'attr' => [
+                    'autocomplete' => 'new-password',
+                    'class' => 'form-control',
+                    "placeholder" => "password"
+                ],
+                'first_options'  => [
+                    'label' => false,
+                    'attr' => [
+                        'class' => 'form-control',
+                        "placeholder" => "password"
+                    ]
+                ],
+                'second_options' => [
+                    'label' => false,
+                    'attr' => [
+                        'class' => 'form-control',
+                        "placeholder" => "c_password"
+                    ]
+                ],
                 'constraints' => [
                     new NotBlank([
                         'message' => 'Please enter a password',
