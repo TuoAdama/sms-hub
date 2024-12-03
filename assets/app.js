0aa26c20a90159c5dea6b27e0c0b24a1e46e41a5
TuoAdama <tuoadama17@gmail.com>
2024-12-03 12:26:18 +0100

    feat: added token generator

--- assets/app.js
+++ assets/app.js
@@ -1,3 +1,5 @@
+import { registerVueControllerComponents } from '@symfony/ux-vue';
+import './bootstrap.js';
 import './styles/app.css';
 import "admin-lte/plugins/fontawesome-free/css/all.min.css"
 import "admin-lte/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css";
@@ -7,6 +9,7 @@ import "admin-lte/dist/css/adminlte.min.css"
 import "admin-lte/plugins/overlayScrollbars/css/OverlayScrollbars.min.css"
 import "admin-lte/plugins/daterangepicker/daterangepicker.css"
 import "admin-lte/plugins/summernote/summernote-bs4.min.css"
+import "toastify-js/src/toastify.css";
 
 import "admin-lte/plugins/jquery/jquery.min.js"
 import "admin-lte/plugins/jquery-ui/jquery-ui.min.js"
@@ -19,3 +22,5 @@ import "admin-lte/plugins/jquery-knob/jquery.knob.min.js"
 import "admin-lte/plugins/daterangepicker/daterangepicker.js"
 import "admin-lte/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"
 import "admin-lte/dist/js/adminlte.js"
+
+registerVueControllerComponents(require.context('./vue/controllers', true, /\.vue$/));
