3a5f8a17771cd7c7d1168fae64fc793b723d6060
TuoAdama <tuoadama17@gmail.com>
2024-12-05 18:30:27 +0100

    feat: added number verification handler

--- src/Controller/NumberController.php
+++ src/Controller/NumberController.php
@@ -4,9 +4,14 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use App\Entity\NumberVerification;
 use App\Entity\User;
 use App\Form\NumberFormType;
+use App\Service\Token\JWTServiceToken;
+use App\Service\Token\TokenGenerator;
+use App\Service\Token\TokenService;
 use Doctrine\ORM\EntityManagerInterface;
+use Random\RandomException;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -16,10 +21,16 @@ use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 class NumberController extends AbstractController
 {
-    public function __construct(private readonly EntityManagerInterface $entityManager)
+    public function __construct(
+        private readonly EntityManagerInterface $entityManager,
+        private readonly TokenService $tokenService,
+    )
     {
     }
 
+    /**
+     * @throws RandomException
+     */
     #[Route('/number/register', name: "app_number_register")]
     #[IsGranted("IS_AUTHENTICATED_FULLY")]
     public function index(#[CurrentUser] User $user, Request $request): Response
@@ -30,11 +41,31 @@ class NumberController extends AbstractController
             $countryCode = $form->get('countryCode')->getData();
             $number = $form->get('number')->getData();
             $user->setNumber("+".$countryCode.$number);
+            $this->handleNumberVerification($user);
             $this->entityManager->flush();
             return $this->redirectToRoute('home');
         }
         return $this->render('pages/number/number_register.html.twig', [
             'form' => $form,
         ]);
+    }
+
+
+    /**
+     * @throws RandomException
+     */
+    private function handleNumberVerification(User $user): void {
+        $numberVerification = $user->getNumberVerification();
+        if ($numberVerification !== null) {
+            $this->entityManager->remove($numberVerification);
+        }
+        $verification = new NumberVerification();
+
+        $verification->setUser($user)
+            ->setVerified(false)
+            ->setToken($this->tokenService->generateNumberVerificationToken($user))
+            ->setTemporalCode(random_int(1000, 9999));
+
+        $this->entityManager->persist($verification);
     }
 }
