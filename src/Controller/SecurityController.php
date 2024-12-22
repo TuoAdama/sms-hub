a7f0d7b4c97842e449d8e0521a75b7dfc3bce1bc
TuoAdama <tuoadama17@gmail.com>
2024-12-22 19:02:59 +0100

    feat: set email verification token

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,18 +2,48 @@
 
 namespace App\Controller;
 
+use App\Form\SendResetPasswordFormType;
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
+use Symfony\Component\Security\Csrf\TokenGenerator\TokenGeneratorInterface;
 
 
 class SecurityController extends AbstractController
 {
-    #[Route('/reset-password', name: 'app_reset_password')]
-    public function resetPassword(): Response
+
+    public function __construct(
+        private readonly TokenGeneratorInterface $tokenGenerator,
+        private readonly EntityManagerInterface $entityManager,
+        private readonly UserRepository $userRepository,
+    )
     {
-        return $this->render('login/password/reset_password.html.twig');
+    }
+
+    #[Route('/reset-password', name: 'app_reset_password')]
+    public function resetPassword(Request $request, TokenGeneratorInterface $tokenGenerator): Response
+    {
+        $form = $this->createForm(SendResetPasswordFormType::class);
+        $isSubmit = false;
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $isSubmit = true;
+            $user = $this->userRepository->findOneBy(['email' => $form->get('email')->getData()]);
+            if ($user) {
+                $token = $tokenGenerator->generateToken();
+                $user->setResetPasswordToken($token);
+                $this->entityManager->flush();
+            }
+        }
+        return $this->render('login/password/reset_password.html.twig', [
+            'form' => $form,
+            'isSubmit' => $isSubmit,
+        ]);
     }
 
     #[Route('/reset-password', name: 'app_reset_password_form')]
