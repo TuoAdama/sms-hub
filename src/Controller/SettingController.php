8281d87527c113805270b6dc3d33f7ae8715c5a8
TuoAdama <tuoadama17@gmail.com>
2025-01-18 17:06:45 +0100

    feat: user edit form

--- src/Controller/SettingController.php
+++ src/Controller/SettingController.php
@@ -2,16 +2,37 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Form\UserFormType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\CurrentUser;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 class SettingController extends AbstractController
 {
-    #[Route('/setting', name: 'app_setting')]
-    public function index(): Response
+
+    public function __construct(
+        private readonly EntityManagerInterface $entityManager,
+    )
     {
-        return $this->render('pages/setting/setting.html.twig');
+    }
+
+    #[Route('/setting', name: 'app_setting')]
+    #[isGranted('IS_AUTHENTICATED_FULLY')]
+    public function index(#[CurrentUser] User $user, Request $request): Response
+    {
+        $form = $this->createForm(UserFormType::class, $user);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->entityManager->flush();
+        }
+        return $this->render('pages/setting/setting.html.twig', [
+            'form' => $form,
+            'user' => $user,
+        ]);
     }
 }
