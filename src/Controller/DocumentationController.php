294a00b47fbf6a7df9a4e0af29fd72d13264949f
TuoAdama <tuoadama17@gmail.com>
2025-03-25 22:34:15 +0100

    refactor: update

--- src/Controller/DocumentationController.php
+++ src/Controller/DocumentationController.php
@@ -2,30 +2,17 @@
 
 namespace App\Controller;
 
-use Parsedown;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\DependencyInjection\Attribute\Autowire;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 class DocumentationController extends AbstractController
 {
-    public function __construct(
-        #[Autowire(param: "project_dir")]
-        private readonly string $projectDir,
-    )
-    {
-    }
-
     #[IsGranted("IS_AUTHENTICATED_FULLY")]
     #[Route('/documentation', name: 'app_documentation')]
     public function index(): Response
     {
-        $parseDown = new Parsedown();
-        $readme = $parseDown->text(file_get_contents($this->projectDir."/README.md"));
-        return $this->render('documentation/index.html.twig', [
-            'readme' => $readme,
-        ]);
+        return $this->render('documentation/index.html.twig');
     }
 }
